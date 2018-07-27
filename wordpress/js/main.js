8bb1e23ae269f65ea9c2afa668878884a75b5c11
Mari-Ogawa <awashimauranaibu@gmail.com>
2018-07-27 18:07:59 +0900

    working on main

--- wordpress/js/main.js
+++ wordpress/js/main.js
@@ -38,20 +38,60 @@ $(function() {
     });
 });
 
+//テキストが終わったら背景を表示する
+
+
+
+
 //一定期間後非表示にする
 setTimeout(function() {
-    $('#text1').fadeOut(3);
-    $('#text2').fadeOut(3);
-    $('#text3').fadeOut(3);
-}, 7);
+    $('#text1').fadeOut(3000);
+    $('#text2').fadeOut(3000);
+    $('#text3').fadeOut(3000);
+}, 7000);
 //3000 と　7000にそれぞれ修正
 
 setTimeout(function() {
     $('#wrapper').fadeIn('slow');
 }, 
-11);
+11000);
 //  11000に修正
 
 
+//画像切替 5秒ごと
 
+$(function(){
+// 設定
+var $width =640; // 横幅
+var $height =300; // 高さ
+var $interval = 5000; // 切り替わりの間隔（ミリ秒）
+var $fade_speed = 2000; // フェード処理の早さ（ミリ秒）
+$("#slide ul li").css({"position":"relative","overflow":"hidden"});
+$("#slide ul li").hide().css({"position":"absolute","top":0,"left":0});
+$("#slide ul li:first").addClass("active").show();
+setInterval(function(){
+var $active = $("#slide ul li.active");
+var $next = $active.next("li").length?$active.next("li"):$("#slide ul li:first");
+$active.fadeOut($fade_speed).removeClass("active");
+$next.fadeIn($fade_speed).addClass("active");
+},$interval);
+});
+
+//クリックされたら表示する
+$(function(){
+    $('#gnavi').hide();
+$('.fa').on('click', function() {
+  $("#gnavi").show();
+});
+});
+
+//クッキー処理
+$(function(){
+    if($.cookie("access")){
+        $('#first').css({display:'none'});
+    }
+    $(window).load(function(){
+        $.cookie("access",$('body').addClass('access'));
+    })
+});
 
